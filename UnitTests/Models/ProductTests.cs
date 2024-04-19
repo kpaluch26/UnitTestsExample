fff74cd236ba67831a2629914b56d42929ab80cb
LGBSPL\kpaluch <kamil.paluch@euvic.pl>
2024-04-19 11:04:16 +0200

    Add project files.

---
+++ UnitTests/Models/ProductTests.cs
@@ -0,0 +1,64 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Tests.Models;
+
+public class ProductTests
+{
+    private Product _product;
+
+    public ProductTests()
+    {
+        _product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Product",
+            Manufacturer = "Test Manufacturer",
+            Price = 99.99,
+            Quantity = 500
+        };
+    }
+
+    [Fact]
+    public void ApplyDiscount_ChangesPriceCorrectly()
+    {
+        // Arrange
+        int discount = 16;
+        double newCorrectPrice = _product.Price - Math.Round(_product.Price * discount / 100, 2);
+
+        // Act
+        _product.ApplyPercentageDiscount(discount);
+
+        // Assert
+        Assert.Equal(newCorrectPrice, _product.Price);
+    }
+
+    [Fact]
+    public void PlaceBulkOrder_ReduceQuantityCorrectly()
+    {
+        // Arrange
+        int orderQuantity = _product.Quantity * 25 / 100;
+        int newCorrectQuantity = _product.Quantity - orderQuantity;
+
+        // Act
+        bool result = _product.PlaceBulkOrder(orderQuantity);
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(newCorrectQuantity, _product.Quantity);
+    }
+
+    [Fact]
+    public void PlaceBulkOrder_FailsWhenQuantityIsTooLarge()
+    {
+        // Arrange
+        int orderQuantity = _product.Quantity + 2;
+        int newCorrectQuantity = _product.Quantity;
+
+        // Act
+        bool result = _product.PlaceBulkOrder(orderQuantity);
+
+        // Assert
+        Assert.False(result);
+        Assert.Equal(newCorrectQuantity, _product.Quantity);
+    }
+}
---
+++ WebStore/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace WebStore.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void ApplyPercentageDiscount(int percentageDiscount)
+        {
+            Price -= Math.Round(Price * percentageDiscount / 100, 2);
+        }
+
+        public bool PlaceBulkOrder(int orderQuantity)
+        {
+            if (orderQuantity > Quantity)
+            {
+                return false;
+            }
+
+            Quantity -= orderQuantity;
+            return true;
+        }
+    }
+}
